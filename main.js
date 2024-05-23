1d55712614e91c3ef6a4ee47999bdbb89a86deef
JoseDelgado1899 <delgadojose213@gmail.com>
2024-05-23 16:46:06 -0500

    feat: :construction: cambios

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-const allDropdown = document.querySelectorAll('#sidebar .side-dropdown');
+const allDropdown = document.querySelectorAll('"sidebar .side-dropdown');
 const sidebar = document.getElementById('sidebar');
 allDropdown.forEach(item=> {
-	const a = item.parentElement.querySelector('a:first-child');
-	a.addEventListener('click', function (e) {
-		e.preventDefault();
-		if(!this.classList.contains('active')) {
-			allDropdown.forEach(i=> {
-				const aLink = i.parentElement.querySelector('a:first-child');
-				aLink.classList.remove('active');
-				i.classList.remove('show');
-			})
-		}
-		this.classList.toggle('active');
-		item.classList.toggle('show');
-	})
+    const a = item.parentElement.querySelector('a:first-child');
+    a.addEventListener('click', function (e) {
+        e.preventDefault();
+        if(!this.classList.contains('active')) {
+            allDropdown.forEach(i=> {
+                const aLink = i.parentElement.querySelector('a:first-child');
+                aLink.classList.remove('active');
+                i.classList.remove('show');
+            })
+        }
+        this.classList.toggle('active');
+        item.classList.toggle('show');
+    })
 })
 
 const toggleSidebar = document.querySelector('nav .toggle-sidebar');
@@ -77,14 +77,14 @@ sidebar.addEventListener("mouseenter", function () {
     }
 }) 
 
-const profile = documente.querySelector("nav .profile");
+const profile = document.querySelector("nav .profile");
 const imgProfile = profile.querySelector("img");
 const dropdownProfile = profile.querySelector(".profile-link");
 imgProfile.addEventListener("click" , function() {
     dropdownProfile.classList.toggle("show");
 })
 
-const allMenu = document.querySelectorAll("main . content-data . head .menu");
+const allMenu = document.querySelectorAll("main .content-data .head .menu");
 allMenu.forEach(item=> {
     const icon = item.querySelector(".icon");
     const menuLink = link.querySelector(".menu-link")
@@ -116,7 +116,7 @@ window.addEventListener('click' , function(e) {
     })
 })
 
-const allProgress = document.querySelectorAll("main . card . progress");
+const allProgress = document.querySelectorAll("main .card .progress");
 allProgress.forEach(item=>{
     item.style.setProperty("--value", item.dataset.value)
 })
